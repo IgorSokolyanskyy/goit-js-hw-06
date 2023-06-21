a979ea37a6cb9581af16b15eba25ea34115acca9
IgorSokolyanskyy <1988isokol@gmail.com>
2023-06-21 20:12:01 +0300

    create fun

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,34 @@
+/** @format */
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, "0")}`;
+}
+
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const boxesContainer = document.getElementById("boxes");
+
+createButton.addEventListener("click", createBoxes);
+destroyButton.addEventListener("click", destroyBoxes);
+
+function createBoxes() {
+  const amount = document.getElementById("inputAmount").value;
+  const boxes = [];
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    box.classList.add("box");
+    boxes.push(box);
+  }
+
+  boxesContainer.append(...boxes);
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
 }
